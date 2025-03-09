008c6830afa0691e50cefedbd60a1db94a29f44f
Zeq <124875024+ZEQ-10@users.noreply.github.com>
2025-03-09 13:23:19 +0700

    lv1

--- 15-classes/exercise/lv2.js
+++ 15-classes/exercise/lv2.js
@@ -23,12 +23,17 @@ class Dog extends Animal{
     constructor(name, age, color, legs){
         super(name, age, color, legs)
     }
-  
+    sound(){
+        return 'woof woof'
+    }
 }
 
 class Cat extends Animal{
     constructor(name, age, color, legs){
         super(name, age, color, legs)
+    }
+    sound(){
+        return 'meow meow'
     }
 }
 
@@ -36,6 +41,6 @@ const d1 = new Dog('Map', 1, 'Brown', 4)
 const c1 = new Cat('Muop', 2, 'Black', 4)
 
 console.log(d1) //Dog { name: 'Map', age: 1, color: 'Brown', legs: 4 }
-console.log(d1.eat()) //Eating
+console.log(d1.sound()) //woof woof
 console.log(c1) //Cat { name: 'Muop', age: 2, color: 'Black', legs: 4 }
-console.log(c1.eat()) //Eating
+console.log(c1.sound()) //meow meow
