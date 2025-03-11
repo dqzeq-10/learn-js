0e82d4d86742d86d6fa5869bbd90fa293971c049
Zeq <124875024+ZEQ-10@users.noreply.github.com>
2025-03-11 16:06:53 +0700

    lv2

---
+++ 17-webStorage/exercise/lv2.js
@@ -0,0 +1,12 @@
+const student = {
+    firstname: 'Quy',
+    lastname: 'Tran',
+    age: 20,
+    skills: [
+        'html', 'css', 'js', 'c#', 'java'
+    ],
+    country: 'Vietnam',
+    city: 'Quangtri'
+}
+
+localStorage.setItem('studentObj', student)
